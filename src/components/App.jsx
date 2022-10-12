8997f678555f4dba63cfaa573e0934c1fc4f631b
DmitriyR90 <ryabokon.dn@gmail.com>
2022-10-12 15:37:25 +0300

    update after remarks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import axios from 'axios';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
-
-axios.defaults.baseURL = 'https://pixabay.com/api';
-const API_KEY = '29525143-9c76bb8aba39698f94cc40e50';
-const PER_PAGE = 12;
+import { getImages, PER_PAGE } from '../Api';
 
 export class App extends Component {
   state = {
@@ -31,24 +27,30 @@ export class App extends Component {
   }
 
   async fetchImages() {
+    const { query, currentPage, images } = this.state;
+
     this.setState({ isLoading: true });
-    const response = await axios.get(
-      `/?q=${this.state.query}&page=${this.state.currentPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
-    );
-    const imagesArray = response.data.hits;
-    const totalHits = response.data.total;
-    const showedHits = this.state.images.length;
 
-    if (imagesArray.length !== 0) {
-      this.setState(prevState => ({
-        images: [...prevState.images, ...imagesArray],
-      }));
-      this.setState({ showLoadMore: true });
-      this.setState({ isLoading: false });
-    }
+    try {
+      const response = await getImages(query, currentPage);
+      const imagesArray = response.hits;
+      const totalHits = response.total;
+      const showedHits = images.length;
 
-    if (totalHits <= showedHits || totalHits <= PER_PAGE) {
-      this.setState({ showLoadMore: false });
+      if (imagesArray.length !== 0) {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...imagesArray],
+        }));
+        this.setState({ showLoadMore: true });
+      }
+
+      if (totalHits <= showedHits || totalHits <= PER_PAGE) {
+        alert(`No more results for ${query}`);
+        this.setState({ showLoadMore: false });
+      }
+    } catch {
+      alert('Something went wrong. Try again');
+    } finally {
       this.setState({ isLoading: false });
     }
   }
